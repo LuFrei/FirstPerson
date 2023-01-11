6018335964a854116360fb786ce1981e59e1a085
LuFrei <32626445+LuFrei@users.noreply.github.com>
2023-01-11 16:49:44 -0500

    init commit

---
+++ Assets/Scripts/PlayableCharacter.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayableCharacter : MonoBehaviour {
+    public int health;
+    public int speed;
+    public int jumpForce;
+}
